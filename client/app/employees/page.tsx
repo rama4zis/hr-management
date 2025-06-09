b9546057c6581737e0f7427f5b3bde4d562af162
rama <rama.4ditya3@gmail.com>
2025-06-09 12:16:31 +0700

    Employees table 🧑‍💼

--- client/app/employees/page.tsx
+++ client/app/employees/page.tsx
@@ -1,109 +1,433 @@
+"use client";
+
 import {
+  Box,
   Container,
   Input,
+  Pagination,
   Paper,
+  Stack,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  TableSortLabel,
 } from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
 import Image from "next/image";
+import React from "react";
 
-export default function employees() {
+interface Employee {
+  id: number;
+  picture: string;
+  name: string;
+  email: string;
+  phone: string;
+  position: string;
+  department: string;
+  salary: number;
+  hireDate: string;
+  status: string;
+}
+
+function createData(
+  id: number,
+  picture: string,
+  name: string,
+  email: string,
+  phone: string,
+  position: string,
+  department: string,
+  salary: number,
+  hireDate: string,
+  status: string
+): Employee {
+  return {
+    id,
+    picture,
+    name,
+    email,
+    phone,
+    position,
+    department,
+    salary,
+    hireDate,
+    status,
+  };
+}
+
+const employeesData = [
+  {
+    id: 1,
+    picture: "/profile.jpg",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "123-456-7890",
+    position: "Software Engineer",
+    department: "IT",
+    salary: 60000,
+    hireDate: "2023-01-15",
+    status: "Active",
+  },
+  {
+    id: 2,
+    picture: "/profile.jpg",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "987-654-3210",
+    position: "Project Manager",
+    department: "Management",
+    salary: 75000,
+    hireDate: "2023-02-01",
+    status: "Active",
+  },
+  {
+    id: 3,
+    picture: "/profile.jpg",
+    name: "Peter Jones",
+    email: "peter.jones@example.com",
+    phone: "555-123-4567",
+    position: "HR Specialist",
+    department: "Human Resources",
+    salary: 55000,
+    hireDate: "2023-03-10",
+    status: "Inactive",
+  },
+];
+
+let rows = [];
+for (const employee of employeesData) {
+  rows.push(
+    createData(
+      employee.id,
+      employee.picture,
+      employee.name,
+      employee.email,
+      employee.phone,
+      employee.position,
+      employee.department,
+      employee.salary,
+      employee.hireDate,
+      employee.status
+    )
+  );
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Employee;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "picture",
+    numeric: false,
+    disablePadding: true,
+    label: "Picture",
+  },
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: false,
+    label: "Name",
+  },
+  {
+    id: "email",
+    numeric: false,
+    disablePadding: false,
+    label: "Email",
+  },
+  {
+    id: "phone",
+    numeric: false,
+    disablePadding: false,
+    label: "Phone",
+  },
+  {
+    id: "position",
+    numeric: false,
+    disablePadding: false,
+    label: "Position",
+  },
+  {
+    id: "department",
+    numeric: false,
+    disablePadding: false,
+    label: "Department",
+  },
+  {
+    id: "salary",
+    numeric: true,
+    disablePadding: false,
+    label: "Salary",
+  },
+  {
+    id: "hireDate",
+    numeric: false,
+    disablePadding: false,
+    label: "Hire Date",
+  },
+  {
+    id: "status",
+    numeric: false,
+    disablePadding: false,
+    label: "Status",
+  },
+];
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Employee
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+export function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler =
+    (property: keyof Employee) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export default function Employees() {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<keyof Employee>("name");
+  const [selected, setSelected] = React.useState<readonly number[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Employee
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, id: number) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected: readonly number[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage - 1);
+  };
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+    setPage(0);
+  };
+
+  const isSelected = (id: number) => selected.indexOf(id) !== -1;
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      rows
+        .filter((row) =>
+          Object.values(row).some((value) =>
+            String(value).toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        )
+        .sort(getComparator(order, orderBy))
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [order, orderBy, page, rowsPerPage, searchTerm]
+  );
+
   return (
     <Container>
-      {/* Search Employee */}
-      <div className="flex items-center justify-center mb-8">
+      {/* Search */}
+      <div className="flex justify-center items-center">
         <input
+          className="p-3 border border-gray-300 rounded-full focus:outline-none focus:ring-blue-500 focus:border-blue-500 w-1/2 my-4 transition duration-300 ease-in-out"
           type="text"
-          className="w-full max-w-md p-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
-          placeholder="Search Employee"
+          placeholder="Search employees"
+          value={searchTerm}
+          onChange={handleSearch}
+          // sx={{ m: 2 }}
         />
       </div>
-      {/* Table Employee List */}
-      <div className="mt-4">
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Picture</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Email</TableCell>
-                <TableCell align="right">Phone</TableCell>
-                <TableCell align="right">Position</TableCell>
-                <TableCell align="right">Department</TableCell>
-                <TableCell align="right">Actions</TableCell>
-                <TableCell align="right">Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell component="th" scope="row">
-                  <Image
-                    src="/profile.jpg"
-                    alt="Picture"
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                  />
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  John Doe
-                </TableCell>
-                <TableCell align="right">john.doe@example.com</TableCell>
-                <TableCell align="right">123-456-7890</TableCell>
-                <TableCell align="right">Software Engineer</TableCell>
-                <TableCell align="right">IT</TableCell>
-                <TableCell align="right">Edit</TableCell>
-                <TableCell align="right">Delete</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell component="th" scope="row">
-                  <Image
-                    src="/profile.jpg"
-                    alt="Picture"
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                  />
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  Jane Smith
-                </TableCell>
-                <TableCell align="right">jane.smith@example.com</TableCell>
-                <TableCell align="right">987-654-3210</TableCell>
-                <TableCell align="right">Project Manager</TableCell>
-                <TableCell align="right">Management</TableCell>
-                <TableCell align="right">Edit</TableCell>
-                <TableCell align="right">Delete</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell component="th" scope="row">
-                  <Image
-                    src="/profile.jpg"
-                    alt="Picture"
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                  />
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  Peter Jones
-                </TableCell>
-                <TableCell align="right">peter.jones@example.com</TableCell>
-                <TableCell align="right">555-123-4567</TableCell>
-                <TableCell align="right">HR Specialist</TableCell>
-                <TableCell align="right">Human Resources</TableCell>
-                <TableCell align="right">Edit</TableCell>
-                <TableCell align="right">Delete</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-      {/* Add Employee Button */}
+
+      <Box sx={{ width: "100%" }}>
+        <Paper sx={{ width: "100%", mb: 2 }}>
+          <TableContainer className="p-4">
+            <Table
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size="medium"
+            >
+              <EnhancedTableHead
+                numSelected={selected.length}
+                order={order}
+                orderBy={orderBy}
+                onSelectAllClick={handleSelectAllClick}
+                onRequestSort={handleRequestSort}
+                rowCount={rows.length}
+              />
+              <TableBody>
+                {visibleRows.map((row, index) => {
+                  const isItemSelected = isSelected(row.id);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.id}
+                      selected={isItemSelected}
+                      sx={{ cursor: "pointer" }}
+                    >
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        <Image
+                          src={row.picture}
+                          alt="Profile
+                          picture"
+                          width={40}
+                          height={40}
+                          className="rounded-full"
+                        />
+                      </TableCell>
+                      <TableCell align="left">{row.name}</TableCell>
+                      <TableCell align="left">{row.email}</TableCell>
+                      <TableCell align="left">{row.phone}</TableCell>
+                      <TableCell align="left">{row.position}</TableCell>
+                      <TableCell align="left">{row.department}</TableCell>
+                      <TableCell align="right">{row.salary}</TableCell>
+                      <TableCell align="left">{row.hireDate}</TableCell>
+                      <TableCell align="left">{row.status}</TableCell>
+                    </TableRow>
+                  );
+                })}
+                {emptyRows > 0 && (
+                  <TableRow
+                    style={{
+                      height: 53 * emptyRows,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          {/* Pagination */}
+          <div className="flex justify-center items-center">
+            <Stack spacing={2} sx={{ p: 2 }}>
+              <Pagination
+                count={Math.ceil(
+                  rows.filter((row) =>
+                    Object.values(row).some((value) =>
+                      String(value)
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    )
+                  ).length / rowsPerPage
+                )}
+                page={page + 1}
+                onChange={handleChangePage}
+                color="primary"
+              />
+            </Stack>
+          </div>
+        </Paper>
+      </Box>
     </Container>
   );
 }
