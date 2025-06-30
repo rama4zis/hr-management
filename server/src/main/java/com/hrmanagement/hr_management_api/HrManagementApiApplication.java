e78805b06c337b88779c2f385769ee71866aa656
rama <rama.4ditya3@gmail.com>
2025-06-30 15:24:06 +0700

    feat: initialize HR management API with Maven setup and basic application structure

---
+++ server/src/main/java/com/hrmanagement/hr_management_api/HrManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.hrmanagement.hr_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/hrmanagement/hr_management_api/HrManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmanagement.hr_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
